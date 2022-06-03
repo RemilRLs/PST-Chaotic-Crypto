d966a777cb94ed1e116488c6863b88ec034839d5
Remil <51848204+RemilRLs@users.noreply.github.com>
2022-06-03 16:37:02 +0200

    Add Decrypt File

--- interface.py
+++ interface.py
@@ -2,46 +2,97 @@ from tkinter import *
 from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 import os
+from turtle import width
 import keygen as kg
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-def encrypt_file(filename):
-    imgToEncrypt = mpimg.imread(filename)
-    plt.imshow(imgToEncrypt) # Génération de l'image à l'aide d'une structure.
-    plt.show() # Permet d'afficher celle-ci.
 
 
-    height = imgToEncrypt.shape[0]
-    width = imgToEncrypt.shape[1] # On récupère dans notre structure les informations t-elle que la hauteur et la largeur.
+def encrypt_file(filename, yesorno, encryptFile):
+
+    if(yesorno):
+        imgToEncrypt = mpimg.imread(filename)
+        plt.imshow(imgToEncrypt) # Génération de l'image à l'aide d'une structure.
+        plt.show() # Permet d'afficher celle-ci.
 
 
-    # Génération de la clé :
-    # 0.01 : Paramètre d'initialisation.
-    # 3.95 : Notre mu (ici la suite sera totalement chaotique (cycle très attractif)).
-    # width*height : On donne la largeur * la hauteur de l'image afin de subsituer chacun des pixels.
-
-    key = kg.keygen(0.01, 3.95, height*width)
+        height = imgToEncrypt.shape[0]
+        width = imgToEncrypt.shape[1] # On récupère dans notre structure les informations t-elle que la hauteur et la largeur.
 
 
-    a = 0
+        # Génération de la clé :
+        # 0.01 : Paramètre d'initialisation.
+        # 3.95 : Notre mu (ici la suite sera totalement chaotique (cycle très attractif)).
+        # width*height : On donne la largeur * la hauteur de l'image afin de subsituer chacun des pixels.
 
-    # np.zeros va créer une liste vide (initialiser à 0) de la taille de notre image
+        key = kg.keygen(0.01, 3.95, height*width)
+        print(key)
 
-    encryptedImg = np.zeros(shape=[height, width,4], dtype = np.uint8)
+        a = 0
 
-    for i in range(height):
-        for j in range (width):
-            encryptedImg[i, j] = imgToEncrypt[i,j] ^ key[a]
-            a += 1
+        # np.zeros va créer une liste vide (initialiser à 0) de la taille de notre image
 
-    plt.imshow(encryptedImg)
-    plt.show()
+        encryptedImg = np.zeros(shape=[height, width,4], dtype = np.uint8)
 
-def select_file():
+        for i in range(height):
+            for j in range (width):
+                encryptedImg[i, j] = imgToEncrypt[i,j] ^ key[a]
+                a += 1
+
+
+        plt.imsave('image/encrypt.bmp', encryptedImg)
+        
+        encryptFile = encryptedImg
+        plt.imshow(encryptFile)
+        plt.show()
+
+        return encryptedImg
+    
+    else:
+            
+        plt.imshow(encryptFile)
+        plt.show()
+
+        height = encryptFile.shape[0]
+        width = encryptFile.shape[1] # On récupère dans notre structure les informations t-elle que la hauteur et la largeur.
+
+        print(height, width)
+
+        key = kg.keygen(0.01, 3.95, height*width)
+        print(key)
+        
+        # Génération de la clé :
+        # 0.01 : Paramètre d'initialisation.
+        # 3.95 : Notre mu (ici la suite sera totalement chaotique (cycle très attractif)).
+        # width*height : On donne la largeur * la hauteur de l'image afin de subsituer chacun des pixels.
+
+        a = 0
+
+        
+        decryptedImg = np.zeros(shape=[height, width,4], dtype = np.uint8)
+
+        for i in range(height):
+            for j in range(width):
+
+                # On refait un XOR par dessus afin de revenir en arrière.
+                decryptedImg[i, j] = encryptFile[i, j] ^ key[a]
+                a += 1
+
+        plt.imshow(decryptedImg)
+        plt.show()
+        plt.imsave('image/decrypt.bmp', decryptedImg)
+
+
+
+
+
+
+
+def select_fileEncrypt():
     filetypes = (
-        ('text files', '*.png'),
+        ('text files', '*.bmp'),
         ('All files', '*.*')
     )
 
@@ -55,8 +106,31 @@ def select_file():
         message=filename
     )
 
-    print(filename)
-    encrypt_file(filename)
+
+    global encryptFile 
+    encryptFile = 1
+    encryptFile = encrypt_file(filename, 1, encryptFile)
+
+
+
+def select_fileDecrypt():
+    filetypes = (
+        ('text files', '*.bmp'),
+        ('All files', '*.*')
+    )
+
+    filename = fd.askopenfilename(
+        title='Open a file',
+        initialdir='/',
+        filetypes=filetypes)
+
+    showinfo(
+        title='Selected File',
+        message=filename
+    )
+    
+    encrypt_file(filename, 0, encryptFile)
+
 
 # Creation of the interface.
 
@@ -76,19 +150,28 @@ window.config(background="white")
 label_title = Label(window, text= "File Encryption", font=("Arrial bold", 17), bg='white', fg='#0070FF')
 label_title.pack(side=TOP)
 
-# Open file button.
+# Open file encrypt button.
 
-open_button = Button(
+open_buttonEncrypt = Button(
     window,
     text= "Encrypt file",
-    command= select_file
+    command= select_fileEncrypt
+    
+)
+
+# Decrypt button.
+
+open_buttonDecrypt = Button(
+    window,
+    text= "Decrypt file",
+    command= select_fileDecrypt
     
 )
 
 
+open_buttonEncrypt.pack(expand=True)
+open_buttonDecrypt.pack(expand=True)
 
-
-open_button.pack(expand=True)
 
 
 
